ac3b72a68cc2c2aa779ad3d4a9bad50c1090a216
AreeyaTripathee <tripatheeareeya79@gmail.com>
2024-12-06 12:02:27 +0545

    YOO

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ const App = () => {
     <div>
       <h1>Hello World</h1>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse amet nulla quia. Quasi eum at dignissimos aliquid facilis temporibus aliquam nulla deleniti corporis repudiandae! Unde a iste et doloremque ducimus?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+         Esse amet nulla quia. Quasi eum at dignissimos aliquid facilis temporibus 
+         aliquam nulla deleniti corporis repudiandae! Unde a iste et doloremque ducimus?
       </p>
     </div>
   )
