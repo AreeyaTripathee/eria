9c38cc4f8326134b96c98b36805e39095dbf1024
AreeyaTripathee <tripatheeareeya79@gmail.com>
2024-12-06 11:58:06 +0545

    HEY

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <h1>HEllo</h1>
+    <div>
+      <h1>Hello World</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse amet nulla quia. Quasi eum at dignissimos aliquid facilis temporibus aliquam nulla deleniti corporis repudiandae! Unde a iste et doloremque ducimus?
+      </p>
+    </div>
   )
 }
 
